48c9430ed45cff73491f22a51610b9a50d95aeac
Tyler Pottle <tyler.pottle@gmail.com>
2015-05-22 10:04:43 -0700

    quick solution for single number problem using ^ bitwise operator

--- lib/single_number.rb
+++ lib/single_number.rb
@@ -1,6 +1,10 @@
 def single_number(array)
   # Given an array of integers, every element appears twice except for one.
   # Find that single one.
-  #
-  #
+  array.inject(0) { |total, num| total ^ num }
 end
+
+
+x = [1, 2, 1, 2, 4, 4, 3]
+
+p single_number(x)
