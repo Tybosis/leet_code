70d5708a48f6ff34b83c4f9fc428973b7469197a
Tyler Pottle <tyler.pottle@gmail.com>
2015-05-22 18:35:07 -0700

    fix typo in method name

--- lib/gcd.rb
+++ lib/gcd.rb
@@ -1,10 +1,10 @@
 # given two positive integers, write a program that returns the greatest
 # common divisor of the two integers.
 #
-def greatest_common_divison(x, y)
+def greatest_common_divisor(x, y)
   first = [x, y].max
   second = [x, y].min
   return first if second == 0
   rem = first - (first / second) * second
-  greatest_common_divison(second, rem)
+  greatest_common_divisor(second, rem)
 end
--- spec/gcd_spec.rb
+++ spec/gcd_spec.rb
@@ -1,25 +1,25 @@
 require 'spec_helper'
 require 'gcd'
 
-describe 'greatest common divison' do
+describe 'greatest common divisor' do
   it 'returns other number if one number is zero' do
-    greatest_common_divison(10, 0).must_equal 10
-    greatest_common_divison(0, 10).must_equal 10
+    greatest_common_divisor(10, 0).must_equal 10
+    greatest_common_divisor(0, 10).must_equal 10
   end
 
   it 'works on two small numbers' do
-    greatest_common_divison(8, 12).must_equal 4
+    greatest_common_divisor(8, 12).must_equal 4
   end
 
   it 'works for two large numbers' do
-    greatest_common_divison(800, 460).must_equal 20
+    greatest_common_divisor(800, 460).must_equal 20
   end
 
   it 'works for two even larger numbers' do
-    greatest_common_divison(42000,9425).must_equal 25
+    greatest_common_divisor(42000,9425).must_equal 25
   end
 
   it 'works for one large and one small number' do
-    greatest_common_divison(1000, 5).must_equal 5
+    greatest_common_divisor(1000, 5).must_equal 5
   end
 end
