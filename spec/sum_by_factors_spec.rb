889c15bdceef390c39066ce1a535b692a5fd4f26
Tyler Pottle <tyler.pottle@gmail.com>
2015-06-08 16:58:04 -0700

    even more tests

--- spec/sum_by_factors_spec.rb
+++ spec/sum_by_factors_spec.rb
@@ -23,4 +23,19 @@ describe 'sum by factors method' do
     set = [107, 158, 204, 100, 118, 123, 126, 110, 116, 100]
     sumOfDivided(set).must_equal correct
   end
+
+  it 'should work for all negative numbers' do
+    correct = [[2, -61548], [3, -4209], [5, -28265], [23, -4209], [31, -31744],
+               [53, -72769], [61, -4209], [1373, -72769], [5653, -28265],
+               [7451, -29804]]
+    sumOfDivided([-29804, -4209, -28265, -72769, -31744]).must_equal correct
+  end
+
+  it 'should work when numbers cancel out' do
+    sumOfDivided([17, -17, 51, -51]).must_equal [[3, 0], [17, 0]]
+  end
+
+  it 'should work for only one number' do
+    sumOfDivided([173471]).must_equal [[41, 173471], [4231, 173471]]
+  end
 end
