3b08a758b1c1141f3537b16019770ce6168d316d
Tyler Pottle <tyler.pottle@gmail.com>
2015-08-01 13:37:22 -0700

    rename method to not conflict with rotate array method

--- spec/rotate_matrix_spec.rb
+++ spec/rotate_matrix_spec.rb
@@ -4,11 +4,11 @@ require 'rotate_matrix'
 describe 'rotate matrix' do
   it 'should work for a simple matrix clockwise' do
     matrix = [[1, 2], [3, 4]]
-    rotate(matrix, 'clockwise').must_equal [[3, 1], [2, 4]]
+    rotate_matrix(matrix, 'clockwise').must_equal [[3, 1], [4, 2]]
   end
 
   it 'should work for a simple matrix counter-clockwise' do
     matrix = [[1, 2], [3, 4]]
-    rotate(matrix, 'counter-clockwise').must_equal [[2, 4], [1, 3]]
+    rotate_matrix(matrix, 'counter-clockwise').must_equal [[2, 4], [1, 3]]
   end
 end
