3befd9eb13078cc1961dac1b7a70b95e0b90861b
Tyler Pottle <tyler.pottle@gmail.com>
2015-08-01 13:28:22 -0700

    specs for rotate matrix problem

---
+++ lib/rotate_matrix.rb
@@ -0,0 +1,6 @@
+# Write a rotate function that rotates a two-dimensional array (a matrix)
+# either clockwise or anti-clockwise by 90 degrees, and returns the rotated array.
+
+# The function accepts two parameters: an array, and a string specifying the
+# direction or rotation. The direction will be either
+# "clockwise" or "counter-clockwise".
