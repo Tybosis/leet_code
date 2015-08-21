ac1e3617f85701dbb4d0001c51adda822258ccb2
Tyler Pottle <tyler.pottle@gmail.com>
2015-08-21 08:19:51 -0700

    arr2bin method working

--- lib/array2binary.rb
+++ lib/array2binary.rb
@@ -4,3 +4,7 @@
 # Note:The binary equivalent should be in string and the array should contain
 # only number otherwise return false.
 #
+def arr2bin(arr)
+  return false if arr.select { |n| n.class != Fixnum }.size > 0
+  arr.empty? ? "0" : arr.inject(:+).to_s(2)
+end
