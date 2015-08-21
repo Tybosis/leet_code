84824d9c67091cf2729b35d861e9985e315bf72a
Tyler Pottle <tyler.pottle@gmail.com>
2015-08-21 08:23:30 -0700

    added more example input to tests

--- spec/array2binary_spec.rb
+++ spec/array2binary_spec.rb
@@ -17,9 +17,20 @@ describe 'array2binary' do
              82, 30, 5, 45, 72, 90, 41, 72, 78, 34, 42, 20, 82, 22, 83, 37, 6,
              5, 91, 14, 61, 7, 85, 92, 30])
     .must_equal "11110000110"
+
+    arr2bin([56, 95, 54, 69, 42, 69, 8, 13, 85, 21]).must_equal "1000000000"
+
+    arr2bin([28, 32, 41, 77, 24, 48, 99, 76, 58, 45, 62, 56, 58, 74, 60])
+    .must_equal "1101000110"
+
+    arr2bin([7, 42, 71, 3, 4, 51, 18, 60, 90, 90, 79, 89, 34, 43, 12, 18, 33,
+             37, 12, 86, 90, 30])
+    .must_equal "1111100111"
   end
 
   it "must return false if array contains a non integer" do
     arr2bin([1, 1, "hello"]).must_equal false
+
+    arr2bin([1, true, {}]).must_equal false
   end
 end
