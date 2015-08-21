9615cb8d4edb3f7dedb1ac5c5e3c9c0740a78b43
Tyler Pottle <tyler.pottle@gmail.com>
2015-08-21 08:19:20 -0700

    initial commit arr2bin file

---
+++ lib/array2binary.rb
@@ -0,0 +1,6 @@
+# Given an array containing only numbers, add all the elements and return the
+# binary equivalent of that sum.
+
+# Note:The binary equivalent should be in string and the array should contain
+# only number otherwise return false.
+#
